7c194e3028d51d3f909e814b0569ee6c2fb216e0
Eric Lopes Rizzatti <eric.rizzatti@gmail.com>
2025-02-10 14:38:11 -0300

    last commit

--- app.js
+++ app.js
@@ -1,35 +1,60 @@
 let amigos = [];
-console.log(amigos);
 let contador = 0;
 
 function exibirTextoNaTela(tag, texto) {
     // Cria um novo elemento <li>
-    let novoItem = document.createElement('li');
-    novoItem.textContent = texto;
+    let novoLinha = document.createElement('li');
+    novoLinha.textContent = texto;
     
     // Adiciona o novo <li> à lista (ul)
     let campo = document.querySelector(tag);
-    campo.appendChild(novoItem);
+    campo.appendChild(novoLinha);
+    console.log (amigos)
 }
 
 function adicionarAmigo() {
-    let nome = document.querySelector('input').value;
+    let nome = document.querySelector('#amigo').value; // Seleciona o input pelo id
 
-    if (nome != '') {
+    if (nome !== '') {
         amigos.push(nome);
         console.log(amigos);
 
-        // Exibe todos os amigos na lista, um abaixo do outro
-        
-            exibirTextoNaTela('ul', amigos[contador]);
-        limparCampo();  // Limpa o campo de input
+        // Exibe o novo amigo na lista
+        exibirTextoNaTela('#listaAmigos', nome); // Adiciona o amigo à lista de amigos
+        limparCampo('#amigo', '');  // Limpa o campo de input
         contador++;  // Atualiza o contador
     } else {
         alert('Por favor, insira um nome.');
     }
 }
 
-function limparCampo() {
-    let nome = document.querySelector('input');
-    nome.value = '';
+function limparCampo(tag, texto) {
+    let nome = document.querySelector(tag);
+    nome.value = texto;
+}
+
+function sortearAmigo() {
+    let campoResultado = document.querySelector('ul');
+    
+    // Remove todos os <li> da lista de resultados
+    while (campoResultado.firstChild) {
+        campoResultado.removeChild(campoResultado.firstChild);
+    }
+
+    if (contador > 0) { // Verifica se há amigos para sortear
+        let resultado = Math.floor(Math.random() * contador); // Sorteia um amigo
+        let textoDaTela = amigos[resultado];
+
+        // Cria um novo <li> para exibir o amigo sorteado
+        let novoResultado = document.createElement('li');
+        novoResultado.classList.add('result-list'); // Adiciona a classe result-list
+        novoResultado.textContent = 'Seu amigo secreto é: ' + textoDaTela; // Define o texto do amigo sorteado
+
+        // Adiciona o novo elemento ao DOM
+        campoResultado.appendChild(novoResultado); // Adiciona o <li> ao <ul> com id "resultado"
+
+        console.log(amigos[resultado]);
+    } else {
+        alert('Adicione amigos antes de sortear!');
+    }
 }
